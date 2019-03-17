fa9b397df7ec1a77501d92b5c44fe1ba77dc6ed6
Arya Putra <rajasa2816@gmail.com>
2019-03-17 16:40:37 +0700

    New file

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            Console.WriteLine("Pilih menu calculator = ");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1. Penambahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Masukkan pilihan = ");
+            int pil = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Masukkan angka = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Masukkan angka = ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (pil == 1)
+            {
+                Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, Penambahan(a, b));
+            }
+            else if (pil == 2)
+            {
+                Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            }
+            else if (pil == 3)
+            {
+                Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            }
+            else if (pil == 4)
+            {
+                Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            }
+
+            Console.WriteLine("\nTekan sembarang tombol untuk keluar");
+            Console.ReadKey();
+        }
+    }
+}
